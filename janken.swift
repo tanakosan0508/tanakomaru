bce8c8a9cbc198444489a7947b92eba6ad0afd4f
tanakosan0508 <32255820+tanakosan0508@users.noreply.github.com>
2019-09-10 09:40:56 +0900

    Update janken.swift

--- janken.swift
+++ janken.swift
@@ -1,20 +1,20 @@
  @IBAction func tapGu(sender: AnyObject) {
-        playerHand.text = "グー"
+        playerHand.text = "グーだしたよ"
     }
     
     @IBAction func tapChoki(sender: AnyObject) {
-        playerHand.text = "チョキー"
+        playerHand.text = "チョキーだバーカ"
     }
     
     @IBAction func tapPa(sender: AnyObject) {
-        playerHand.text = "パー"
+        playerHand.text = "パー！！！！！！！！！！！！！！！！！！！"
     }
     
     //CPU処理
     
      @IBAction func tapGu(sender: AnyObject) {
         :
-        var randomHand = Int(arc4random() % 3)
+        var randomHand = Int(arc4random() % 4)
         cpuHand.text = handName[randomHand]
     }
     
@@ -26,5 +26,7 @@
     
     @IBAction func tapPa(sender: AnyObject) {
         :
-        var randomHand = Int(arc4random() % 3)
+        var randomHand = Int(arc4random() % 2)
         cpuHand.text = handName[randomHand]
+     
+  
